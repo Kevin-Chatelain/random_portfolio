e759fca7990e5160e0db2219c3021a6f96192d1b
Kevin <chatelainkevin9@gmail.com>
2022-12-11 00:21:50 +0100

    ajout bouton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,5 @@
-console.log("jrjrj")
 
-window.addEventListener('wheel', () => {
+
+document.querySelector('.bouton').addEventListener('click', () => {
     document.querySelector('.main').classList.add('up');
-    if(document.querySelector('.main').classList.contains('up')) 
-        window.removeEventListener('wheel', ()=>{})
 })
